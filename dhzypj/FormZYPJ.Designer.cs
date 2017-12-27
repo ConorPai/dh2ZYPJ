9b12979833dd7ac9dae6fd3b667f91576975ad7f
ConorPai <baiguangnan@163.com>
2017-12-27 15:14:23 +0800

    CommitCode

---
+++ dhzypj/FormZYPJ.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace dhzypj
+{
+    partial class FormZYPJ
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQZ = new System.Windows.Forms.TextBox();
+            this.txtNL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtZL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNaiL = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMQ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDD = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPN = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtJD = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblCurZYPJ = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "气质";
+            // 
+            // txtQZ
+            // 
+            this.txtQZ.Location = new System.Drawing.Point(69, 24);
+            this.txtQZ.Name = "txtQZ";
+            this.txtQZ.Size = new System.Drawing.Size(64, 21);
+            this.txtQZ.TabIndex = 1;
+            this.txtQZ.TextChanged += new System.EventHandler(this.txtQZ_TextChanged);
+            // 
+            // txtNL
+            // 
+            this.txtNL.Location = new System.Drawing.Point(211, 24);
+            this.txtNL.Name = "txtNL";
+            this.txtNL.Size = new System.Drawing.Size(64, 21);
+            this.txtNL.TabIndex = 3;
+            this.txtNL.TextChanged += new System.EventHandler(this.txtNL_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "内力";
+            // 
+            // txtZL
+            // 
+            this.txtZL.Location = new System.Drawing.Point(69, 64);
+            this.txtZL.Name = "txtZL";
+            this.txtZL.Size = new System.Drawing.Size(64, 21);
+            this.txtZL.TabIndex = 5;
+            this.txtZL.TextChanged += new System.EventHandler(this.txtZL_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "智力";
+            // 
+            // txtNaiL
+            // 
+            this.txtNaiL.Location = new System.Drawing.Point(211, 64);
+            this.txtNaiL.Name = "txtNaiL";
+            this.txtNaiL.Size = new System.Drawing.Size(64, 21);
+            this.txtNaiL.TabIndex = 7;
+            this.txtNaiL.TextChanged += new System.EventHandler(this.txtNaiL_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "耐力";
+            // 
+            // txtMQ
+            // 
+            this.txtMQ.Location = new System.Drawing.Point(69, 104);
+            this.txtMQ.Name = "txtMQ";
+            this.txtMQ.Size = new System.Drawing.Size(64, 21);
+            this.txtMQ.TabIndex = 9;
+            this.txtMQ.TextChanged += new System.EventHandler(this.txtMQ_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "名气";
+            // 
+            // txtDD
+            // 
+            this.txtDD.Location = new System.Drawing.Point(211, 104);
+            this.txtDD.Name = "txtDD";
+            this.txtDD.Size = new System.Drawing.Size(64, 21);
+            this.txtDD.TabIndex = 11;
+            this.txtDD.TextChanged += new System.EventHandler(this.txtDD_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(180, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "道德";
+            // 
+            // txtPN
+            // 
+            this.txtPN.Location = new System.Drawing.Point(69, 144);
+            this.txtPN.Name = "txtPN";
+            this.txtPN.Size = new System.Drawing.Size(64, 21);
+            this.txtPN.TabIndex = 13;
+            this.txtPN.TextChanged += new System.EventHandler(this.txtPN_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(38, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "叛逆";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(67, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 12);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "需要加点：";
+            // 
+            // txtJD
+            // 
+            this.txtJD.Location = new System.Drawing.Point(129, 197);
+            this.txtJD.Name = "txtJD";
+            this.txtJD.Size = new System.Drawing.Size(61, 21);
+            this.txtJD.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(171, 148);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "当前职业评价：";
+            // 
+            // lblCurZYPJ
+            // 
+            this.lblCurZYPJ.AutoSize = true;
+            this.lblCurZYPJ.Location = new System.Drawing.Point(256, 148);
+            this.lblCurZYPJ.Name = "lblCurZYPJ";
+            this.lblCurZYPJ.Size = new System.Drawing.Size(29, 12);
+            this.lblCurZYPJ.TabIndex = 17;
+            this.lblCurZYPJ.Text = "5070";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(197, 196);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 18;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 242);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblCurZYPJ);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtJD);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPN);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDD);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMQ);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNaiL);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtZL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQZ);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "大话孩子职业评价计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQZ;
+        private System.Windows.Forms.TextBox txtNL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtZL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNaiL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMQ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDD;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPN;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtJD;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblCurZYPJ;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ dhzypj/FormZYPJ.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dhzypj
+{
+    public partial class FormZYPJ : Form
+    {
+        public FormZYPJ()
+        {
+            InitializeComponent();
+        }
+
+        private void txtQZ_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void txtZL_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void txtMQ_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void txtPN_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void txtNL_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void txtNaiL_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void txtDD_TextChanged(object sender, EventArgs e)
+        {
+            ShowCurZYPJ();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowCurZYPJ()
+        {
+            try
+            {
+                int nQZ = string.IsNullOrEmpty(txtQZ.Text) ? 0 : Convert.ToInt32(txtQZ.Text);
+                int nZL = string.IsNullOrEmpty(txtZL.Text) ? 0 : Convert.ToInt32(txtZL.Text);
+                int nNL = string.IsNullOrEmpty(txtNL.Text) ? 0 : Convert.ToInt32(txtNL.Text);
+                int nNaiL = string.IsNullOrEmpty(txtNaiL.Text) ? 0 : Convert.ToInt32(txtNaiL.Text);
+                int nDD = string.IsNullOrEmpty(txtDD.Text) ? 0 : Convert.ToInt32(txtDD.Text);
+                int nPN = string.IsNullOrEmpty(txtPN.Text) ? 0 : Convert.ToInt32(txtPN.Text);
+                int nMQ = string.IsNullOrEmpty(txtMQ.Text) ? 0 : Convert.ToInt32(txtMQ.Text);
+
+                int nZYPJ = CalcZYPJ(nQZ, nZL, nNL, nNaiL, nDD, nPN, nMQ);
+                lblCurZYPJ.Text = nZYPJ.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+        }
+
+        private int CalcZYPJ(int nQZ, int nZL, int nNL, int nNaiL, int nDD, int nPN, int nMQ)
+        {
+            List<double> sx = new List<double>();
+
+            sx.Add(nQZ);
+            sx.Add(nZL);
+            sx.Add(nNL);
+            sx.Add(nNaiL);
+            sx.Add(nDD);
+            sx.Add(nPN);
+            sx.Add(nMQ);
+
+            sx.Sort();
+
+            double nZYPJ = 600.0 * Math.Pow(sx[6], 0.2) + 180.0 * Math.Pow(sx[5], 0.2) + 150.0 * Math.Pow(sx[4], 0.2) + 120.0 * Math.Pow(sx[3], 0.2) + 90.0 * Math.Pow(sx[2], 0.2) + 60.0 * Math.Pow(sx[1], 0.2) + 30.0 * Math.Pow(sx[0], 0.2);
+            return (int)nZYPJ;
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
